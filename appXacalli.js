7ca2f4983c045bd1b5e7cb579a1563120840be40
ghostinit <ghostinit@kaliOs.kaliOs.com>
2021-03-14 20:58:16 -0600

    Empleado y cliente ok

--- appXacalli.js
+++ appXacalli.js
@@ -14,45 +14,10 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
-// COnfiguracion de conexion 
-/*const sequelize = new Sequelize('b70bircd0jwbmntvrazp','uw2ojwdixu6vapak','fH9OeWEfHN5WFyArwNg5',{host:'b70bircd0jwbmntvrazp-mysql.services.clever-cloud.com',dialect:'mysql'})
-//verificamos conexion
-sequelize.authenticate().then(() => {
-	console.log('Conectado');
-})
-.catch(err => {
-	console.log('No se establecio la conexion');
-})
-*/
+
 const db = require('./config/dbConexion.js');
 require('./routes/employee')(app);
 require('./routes/client')(app);
-
-
-
-//--------------------------video
-app.set('view',path.join(__dirname,'view'));
-app.set('public',path.join(__dirname,'public'));
-app.engine('.html', html({
-	defaultLayout:'home',
-	layoutsDir: path.join(app.get('public'),'Img'),
-	layoutsDir: path.join(app.get('public'),'css'),
-	extname:'.html'
-}));
-app.set('view engine','.html');
-//public 
-app.use(express.static(path.join(__dirname, 'public')));
-//--------------------------
-
-
-
-//lo agregamos despues de configurar el index
-//app.use('/',require('./routes'));//lo podemos eliminar--------
-//require('./config/passport');
-//require('./models/mdlEmployee');
-//require('./models/mdlClient');
-
-
 
 //cachamos los errores a 404
 app.use(function(req, res, next){
--- public/js/interaccion.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-	$('.menu li a').on('click', function(event) {
-		event.preventDefault();
-		var href = $(this).attr('href');
-		$('#contenido').load(href);
-	})
-})
